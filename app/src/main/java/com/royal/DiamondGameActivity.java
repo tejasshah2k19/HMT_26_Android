5e1a00ea2bf57a1d357ce3bed5993bf1184feb89
Tejas Shah <tejasshah2k19@gmail.com>
2026-09-23 16:01:55 +0530

    on click

--- app/src/main/java/com/royal/DiamondGameActivity.java
+++ app/src/main/java/com/royal/DiamondGameActivity.java
@@ -15,7 +15,7 @@ public class DiamondGameActivity extends AppCompatActivity {
     //declare
     ImageButton imgBtnDiamondB1,imgBtnDiamondB2,imgBtnDiamondB3,imgBtnDiamondB4,imgBtnDiamondB5,imgBtnDiamondB6,imgBtnDiamondB7,imgBtnDiamondB8,imgBtnDiamondB9;
 
-
+    int blast = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,58 +44,114 @@ public class DiamondGameActivity extends AppCompatActivity {
         imgBtnDiamondB1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB1.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB1.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB1.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
 
         imgBtnDiamondB2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB2.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB2.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB2.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB3.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB3.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB3.setBackgroundResource(R.drawable.blast_hmt);
+                }
+
             }
         });
         imgBtnDiamondB4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB4.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB4.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB4.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB5.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB5.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB5.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB6.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB6.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB6.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB7.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB7.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB7.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB8.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB8.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB8.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
         imgBtnDiamondB9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgBtnDiamondB9.setBackgroundResource(R.drawable.diamond_hmt_512);
+                bomb();//0 1
+                if(blast == 0) {
+                    imgBtnDiamondB9.setBackgroundResource(R.drawable.diamond_hmt_512);
+                }else{
+                    imgBtnDiamondB9.setBackgroundResource(R.drawable.blast_hmt);
+                }
             }
         });
 
+    }
+
+
+    void bomb(){
+        int random  = (int)(Math.random()*10); //6
+        if(random %2 == 0){
+            blast = 0;
+        }else{
+            blast= 1;
+        }
     }
 }
